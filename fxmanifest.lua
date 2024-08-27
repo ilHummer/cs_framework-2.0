724a18e827b62325e062f7311b7b30def3fe358e
Hummer <109071707+ilHummer@users.noreply.github.com>
2024-08-27 23:58:41 +0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'YourName'
+description 'CS Framework - Compatible with QBCORE, QBOX, and ESX'
+version '1.0.0'
+
+shared_script 'cs_core/shared/main.lua'
+client_script 'cs_core/client/main.lua'
+server_script 'cs_core/server/main.lua'
+
+files {
+    'modules/*.lua'
+}
